68d568fc1f413b87adeb40cdfdc48cb47dd5ba19
unknown <leonardocasadiostrozzi@gmail.com>
2025-04-23 17:21:29 +0200

    part3/phonebook added mongo.js 3.12

---
+++ part3/phonebook/mongo.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+    console.log("[!] Password needed as an argument");
+    process.exit(1);
+}
+
+const password = process.argv[2];
+
+//DO NOT SAVE YOUR PASSWORD
+const url = `mongodb+srv://lcs:${password}@phonebook.afhg13q.mongodb.net/notebook?retryWrites=true&w=majority&appName=Phonebook`;
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+});
+const Person = mongoose.model("Person", personSchema);
+
+if (process.argv.length === 5) {
+    const person = new Person({
+        name: process.argv[3],
+        number: process.argv[4],
+    });
+
+    person.save().then((result) => {
+        console.log(`[+] ${process.argv[3]} successfully saved`);
+        mongoose.connection.close(); //closing the connection
+    });
+} else {
+    console.log("Phonebook:");
+    Person.find().then((result) => {
+        result.forEach((person) => {
+            console.log(`${person.name} ${person.number}`);
+            //NOTE: you can't do that outside the .then because it will be executed too soon
+        });
+        mongoose.connection.close(); //closing the connection
+    });
+}
