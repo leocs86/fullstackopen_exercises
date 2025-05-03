73b18b6774c7e2944de812ad9ae7a3aa4657fb8d
unknown <leonardocasadiostrozzi@gmail.com>
2025-05-03 17:11:26 +0200

    fixed required values in BlogScheme

--- part4/bloglist/models/blog.js
+++ part4/bloglist/models/blog.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const blogSchema = mongoose.Schema({
     title: { type: String, required: true },
-    author: { type: String, required: true },
-    url: { type: String },
+    author: { type: String },
+    url: { type: String, required: true },
     likes: { type: Number, default: 0 },
 });
 
